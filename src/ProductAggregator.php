b7cf691eb369529dd10878b06b57ca2935c19efb
unknown <lagoon@gmail.com>
2019-08-17 21:39:30 +0300

    success 5 tests. Problem with rounding float numbers

--- src/ProductAggregator.php
+++ src/ProductAggregator.php
@@ -11,6 +11,10 @@ class ProductAggregator
      */
     private $filename;
 
+    /**
+     * ProductAggregator constructor.
+     * @param string $filename
+     */
     public function __construct(string $filename)
     {
         $this->filename = $filename;
@@ -26,90 +30,88 @@ class ProductAggregator
      */
     public function find(string $currency, int $page, int $perPage): string
     {
-        try {
-            $data  = file_get_contents($this->filename);
-            $info  = json_decode($data);
-            $pages = array_chunk($info->products, $perPage);
+        $data  = file_get_contents($this->filename);        // read data from file
+        $info  = json_decode($data);                        // parse json
+        $pages = array_chunk($info->products, $perPage);    // break the array into pieces
 
-            $products = [];
-            foreach( $pages[$page-1] as $productRaw ) {
-                foreach( $info->categories as $category ) {
+        /*
+        array_unshift($pages,'');
+        unset($pages[0]);
+        */
 
-                    if( $productRaw->category === $category->id ) {
+        if( !array_key_exists($page, $pages) ) throw new UnavailablePageException();
 
-                        $products[] = [
-                            'id'       => $productRaw->id,
-                            'title'    => $productRaw->title,
-                            'category' => [
-                                'id'    => $category->id,
-                                'title' => $category->title,
-                            ],
-                            'price' => [
-                                'amount'           => $productRaw->amount,
-                                'discounted_price' => 0,
-                                'currency'         => $currency,
-                                'discount'         => [
-                                    'type'     => '',
-                                    'value'    => 0.00,
-                                    'relation' => '',
-                                ]
+        $products = []; // future collections of products
+        foreach( $pages[$page-1] as $productRaw ) {
+            foreach( $info->categories as $category ) {
+
+                if( $productRaw->category === $category->id ) { // if matched ids
+
+                    // build needed data
+                    $products[] = [
+                        'id'       => $productRaw->id,
+                        'title'    => $productRaw->title,
+                        'category' => [
+                            'id'    => $category->id,
+                            'title' => $category->title,
+                        ],
+                        'price' => [
+                            'amount'           => $productRaw->amount,
+                            'discounted_price' => 0,
+                            'currency'         => $currency,
+                            'discount'         => [
+                                'type'     => '',
+                                'value'    => 0.00,
+                                'relation' => '',
                             ]
-                        ];
-                    }
+                        ]
+                    ];
                 }
             }
+        }
 
-            $isDiscount = false;
-            $productsCatalog = [];
-            foreach( $products as $index => $product ) {
-                if ($index < count($products)) {
-                    foreach ($info->discounts as $discount) {
+        $isDiscount = false;
+        $productsCatalog = [];
+        foreach( $products as $index => $product ) {
+            if ($index < count($products)) {
+                foreach ($info->discounts as $discount) {
 
-                        if ($product['id'] == $discount->related_id || $product['category']['id'] == $discount->related_id) {
-                            $isDiscount = true;
-                            $discountPrice = $this->setDiscount($discount->type, $product['price']['amount'], $discount->value);
-                            $product['price']['discounted_price']     = $this->currencyConvert($discountPrice, $currency);
-                            $product['price']['discount']['type']     = $discount->type;
-                            $product['price']['discount']['value']    = $discount->value;
-                            $product['price']['discount']['relation'] = $discount->relation;
-                            $productsCatalog[$index] = $product;
-                        }
-                    }
-
-                    if( !$isDiscount ) {
-                        $product['price']['discounted_price'] = $this->currencyConvert($product['price']['amount'], $currency);
+                    if ($product['id'] == $discount->related_id || $product['category']['id'] == $discount->related_id) {
+                        $isDiscount = true;
+                        $discountPrice = $this->setDiscount($discount->type, $product['price']['amount'], $discount->value);
+                        $product['price']['discounted_price']     = $this->currencyConvert($discountPrice, $currency);
+                        $product['price']['discount']['type']     = $discount->type;
+                        $product['price']['discount']['value']    = $discount->value;
+                        $product['price']['discount']['relation'] = $discount->relation;
                         $productsCatalog[$index] = $product;
                     }
-
-                    $isDiscount = false;
                 }
+
+                if( !$isDiscount ) {
+                    $product['price']['discounted_price'] = $this->currencyConvert($product['price']['amount'], $currency);
+                    $productsCatalog[$index] = $product;
+                }
+
+                $isDiscount = false;
             }
-
-            $result['items']   = $productsCatalog;
-            $result['perPage'] = $perPage;
-            $result['pages']   = count($pages[$page]);
-            $result['page']    = $page;
-
-            /*echo '<pre>';
-            print_r($result);
-            echo '</pre>';
-            exit;*/
-
-            return json_encode($result);
-
-        } catch (UnavailablePageException $error ) {
-            return $error;
         }
+
+        $result['items']   = $productsCatalog;
+        $result['perPage'] = $perPage;
+        $result['pages']   = count($pages);
+        $result['page']    = $page;
+
+        return json_encode($result);
     }
 
     /**
-     * Set discount price
+     * Set and return price with discount
      * @param string $type
      * @param float $amount
      * @param float $discount
      * @return float
      */
-    public function setDiscount( string $type, float $amount, float $discount ): string
+    private function setDiscount( string $type, float $amount, float $discount ): float
     {
         switch( $type ) {
             case 'absolute' :
@@ -126,23 +128,24 @@ class ProductAggregator
                 break;
         }
 
-        return number_format(ceil($price * 100) / 100, 2, '.', '');
+        return (float) sprintf("%01.2f", ceil($price * 100) / 100);
     }
 
     /**
      * Converter currencies
      * @param float $amount
      * @param string $currency
+     * @return float
      */
-    public function currencyConvert( float $amount, string $currency )
+    private function currencyConvert( float $amount, string $currency ): float
     {
         if( $currency !== 'UAH' ) {
             $currencies = $this->getCurrencies();
             $curr = $currencies[0]->rates;
 
-            return (float) round(number_format(ceil(($amount * $curr->$currency) * 100) / 100, 3, '.', ''), 2);
+            return (float) round(sprintf("%01.2f", ceil(($amount * $curr->$currency) * 100) / 100), 2);
         } else {
-            return (float) round(number_format(ceil($amount * 100) / 100, 3, '.', ''), 2);
+            return (float) round(sprintf("%01.2f", ceil($amount * 100) / 100), 2);
         }
     }
 
@@ -150,7 +153,7 @@ class ProductAggregator
      * Read currency rates
      * @return mixed
      */
-    public function getCurrencies()
+    private function getCurrencies()
     {
         $data = json_decode(file_get_contents($this->filename));
 
